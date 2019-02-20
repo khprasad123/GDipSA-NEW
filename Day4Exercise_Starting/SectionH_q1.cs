122893b2b9fce0d9bcc50ef0901091e19448be40
Hariprasad Kottarathil <khprasad123@gmail.com>
2019-02-20 16:33:48 +0800

    exercise 1 section H

---
+++ Day4Exercise_Starting/SectionH_q1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Exercise_Starting
+{
+    class SectionH_q1
+    {
+        static void Main()
+        {
+            int num = ReadInteger();
+           Console.WriteLine("Entered Integer:"+num);
+        }
+
+        static int ReadInteger()
+        {
+            bool F = true;
+            while (F)
+            {
+                Console.Write("Enter an Integer : ");
+                var num =int.TryParse( Console.ReadLine(),out int n);
+                if (num)
+                    return n;
+            }
+            return 0;
+        }
+    }
+}
